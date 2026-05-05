ae9973bdf6d4609328395cbef5d0163902987508
MONY🐹 <mounags17@gmail.com>
2026-05-05 20:58:23 +0100

    Create Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,16 @@
+class Rectangle extends Shape {
+    private double length, width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double area() {
+        return length * width;
+    }
+
+    public double perimeter() {
+        return 2 * (length + width);
+    }
+}

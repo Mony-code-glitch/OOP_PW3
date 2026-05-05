24e4ff1495565a80ea4f2b6e42c66e9eea9e1d63
MONY🐹 <mounags17@gmail.com>
2026-05-05 20:52:29 +0100

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,16 @@
+class Student extends Person {
+    private double grade;
+
+    public Student(String name, int age, double grade) {
+        super(name, age);
+        this.grade = grade;
+    }
+
+    public boolean isPassed() {
+        return grade >= 10;
+    }
+
+    public String toString() {
+        return super.toString() + ", Grade: " + grade;
+    }
+}

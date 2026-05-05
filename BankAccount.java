ac1776ce7d29f38f888fc0be81ce25a1297c7f2d
MONY🐹 <mounags17@gmail.com>
2026-05-05 20:51:01 +0100

    Update BankAccount.java

--- BankAccount.java
+++ BankAccount.java
@@ -1,1 +1,25 @@
+class BankAccount {
+    private String ownerName;
+    private double balance;
 
+    public BankAccount(String ownerName, double balance) {
+        this.ownerName = ownerName;
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) balance -= amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String toString() {
+        return "Owner: " + ownerName + ", Balance: " + balance;
+    }
+}

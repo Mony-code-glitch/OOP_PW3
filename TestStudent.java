01ac6a6d368c028b9830f740e3c35bb0e9bf7061
MONY🐹 <mounags17@gmail.com>
2026-05-05 20:53:47 +0100

    Create TestStudent.java

---
+++ TestStudent.java
@@ -0,0 +1,8 @@
+public class TestStudent {
+    public static void main(String[] args) {
+        Student s = new Student("Sara", 20, 12.5);
+
+        System.out.println(s);
+        System.out.println(s.isPassed());
+    }
+}

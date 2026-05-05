5abea2041fe35bb7dbcb76846e7f369792cd1784
MONY🐹 <mounags17@gmail.com>
2026-05-05 20:54:51 +0100

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,17 @@
+class Employee {
+    private String name;
+    private double salary;
+
+    public Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public String toString() {
+        return name + ", " + salary;
+    }
+}
